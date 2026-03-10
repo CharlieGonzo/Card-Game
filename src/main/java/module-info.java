a8cf782d822b29075eb44bd59dc56277353d73b2
Charles Gonzalez Jr <mryankeecj23@gmail.com>
2026-03-09 22:16:36 -0400

    fixed dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,16 +2,6 @@ module org.example.cardgame24 {
     requires javafx.controls;
     requires javafx.fxml;
     requires exp4j;
-    requires java.desktop;
-
-
-    requires java.net.http;
-
-
-    requires java.scripting;
-
-
-
 
     opens org.example.cardgame24 to javafx.fxml;
     exports org.example.cardgame24;
